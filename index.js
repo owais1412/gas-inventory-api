412cfd203f40c9174d0a22d6b508c2129ac4c397
Syed Owais Hussain <owes.hussain@gmail.com>
2022-01-10 21:32:01 +0530

    Add caching for API calls

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cache = require("memory-cache");
+
 if (process.env.DEV == true) {
   require("dotenv").config();
 }
@@ -9,6 +11,29 @@ const HTTP_PORT = process.env.PORT || 8000
 app.listen(HTTP_PORT, '0.0.0.0', () => {
     console.log("Server is listening on port " + HTTP_PORT);
 });
+
+// set up using cache middleware for 10 minutes
+const memCache = new cache.Cache();
+const cacheMiddleware = (duration) => {
+    return (req, res, next) => {
+        let key =  '__express__' + req.originalUrl || req.url;
+        let cacheContent = memCache.get(key);
+        if(cacheContent){
+            console.log("Cache hit for " + key);
+            res.send(cacheContent);
+            return;
+        }else{
+            res.sendResponse = res.send;
+            res.send = (body) => {
+                memCache.put(key, body, duration * 1000);
+                res.sendResponse(body);
+            }
+            next();
+        }
+    }
+}
+
+app.use(cacheMiddleware(60));
 
 app.use(express.json());
 
